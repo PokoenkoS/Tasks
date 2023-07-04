23a9b27e53570b4b025b1028f97483d44afcd096
PokoenkoS <pokoenkosveta@gmail.com>
2023-07-04 13:18:20 +0300

    add reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,15 @@
+import { statusFilters } from "./constants";
+
+const taskInitialState =[];
+
+export const taskReducer = (state=taskInitialState, action)=>{
+
+}
+
+const filterInitialState = {
+    status: statusFilters.all,
+}
+
+export const filterReducer = (state=filterInitialState, action) =>{
+    
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { rootReducer } from "./reducer";
+import { filterReducer, taskReducer } from "./reducer";
 
  export const store = configureStore({
-    reducer:rootReducer,
+    reducer:{
+        task: taskReducer,
+        filter: filterReducer,
+    }
 })
