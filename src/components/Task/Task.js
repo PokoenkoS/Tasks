df579ad115dc449b36a3685c3cc1ccc3e36e07cc
PokoenkoS <pokoenkosveta@gmail.com>
2023-07-04 18:18:31 +0300

    add tasks

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,15 +1,25 @@
+import { useDispatch } from "react-redux"
 import css from "./Task.module.css"
+import { deleteTask, toggleCompleted } from "redux/actions";
 
 export const Task =({task})=>{
+    const dispatch = useDispatch();
+
+    const handleDelete = () => dispatch(deleteTask(task.id));
+    const handleToggle =()=> dispatch(toggleCompleted(task.id))
+
+    
+
 return(
     <div className={css.wrapper}>
         <input
         className={css.checkbox}
         type="checkbox"
         checked={task.completed}
+        onClick={handleToggle}
         />
         <p className={css.text}>{task.text}</p>
-        <button className={css.btn}> close</button>
+        <button className={css.btn} onClick={handleDelete}> close</button>
     </div>
 )
 }
