50a2a9638aecce50b03c2a450b944f1dc3796868
PokoenkoS <pokoenkosveta@gmail.com>
2023-07-05 12:46:20 +0300

    changed statusFilter

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,30 @@
 import { Button } from "components/Button/Button"
 import css from "./StatusFilter.module.css"
+import { useDispatch, useSelector } from "react-redux"
+import { getStatusFilter } from "redux/selectors";
+import { setStatusFilter } from "redux/actions";
+import { statusFilters } from "redux/constants";
 
 export const StatusFilter =()=> {
+    const dispatch = useDispatch();
+    const filter = useSelector(getStatusFilter);
+
+    const handleFilterChange = ()=>{
+        dispatch(setStatusFilter(filter))
+    }
     return(
         <div className={css.wrapper}>
-            <Button>All</Button>
-            <Button>Active</Button>
-            <Button>Completed</Button>
+            <Button
+             onClick={()=>handleFilterChange(statusFilters.all)}
+             selected={filter ===statusFilters.all} 
+             
+             >All</Button>
+            <Button
+            onClick={()=>handleFilterChange(statusFilters.active)}
+            selected={filter ===statusFilters.active}>Active</Button>
+            <Button
+            onClick={()=>handleFilterChange(statusFilters.completed)}
+            selected={filter ===statusFilters.active}>Completed</Button>
         </div>
     )
 }
--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,16 +1,22 @@
+import { useDispatch } from "react-redux";
+import { addTask } from "redux/actions";
+
 export const TaskForm =()=>{
-    const handlesSubmit = (e)=>{
+const dispatch = useDispatch();
+
+    const handleSubmit = (e)=>{
         e.preventDefault();
         const form = e.target;
+        dispatch(addTask(form.elements.text.value))
        form.reset();
     }
     return(
-<form>
+<form  onSubmit={handleSubmit}>
     <input
     type="text"
     name="text"
     placeholder="Enter task text..."
-    onSubmit={handlesSubmit}
+   
    />
    <button type="submit">Add task</button>
 </form>
