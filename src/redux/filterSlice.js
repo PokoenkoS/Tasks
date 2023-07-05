8f3f2d08084a0e68dad7e7b3ec9469f65a45f265
PokoenkoS <pokoenkosveta@gmail.com>
2023-07-05 19:08:42 +0300

    add filterSlice

---
+++ src/redux/filterSlice.js
@@ -0,0 +1,19 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const filterInitialState = {
+    status: statusFilters.all,
+}
+
+export const filterSlice = createSlice({
+    name: 'filter',
+    initialState: filterInitialState,
+    reducers: {
+        setStatusFilter(state, action) {
+            state.status=action.payload;
+        },
+    },
+
+});
+
+export const {setStatusFilter} = filterSlice.actions;
+export const filtersReducer = filterSlice.reducer;
